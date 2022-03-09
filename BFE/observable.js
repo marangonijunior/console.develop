bafababc6a0b22b32fb5d13f5f1e20e01c7c68e1
Hednei C Marangoni Junior <marangonijunior@gmail.com>
2022-03-09 16:19:02 +0000

    Update observable.js

--- BFE/observable.js
+++ BFE/observable.js
@@ -1,32 +1,33 @@
 class Observable {
-  constructor(setup){
-        this._setup = setup;
-    }
-
-    subscribe(subscriber) {
-        const sub_wrapper = {
-            unsubscribed: false,
-            next(val){
-                if(this.unsubscribed) return;
-                if (subscriber instanceof Function) return subscriber(val);
-                return subscriber.next ? subscriber.next(val) : null;
-            },
-            error(val){
-                if(this.unsubscribed) return;
-                this.unsubscribe();
-                return subscriber.error ? subscriber.error(val) : null;
-            },
-            complete(){
-                if(this.unsubscribed) return;
-                this.unsubscribe();
-                return subscriber.complete ? subscriber.complete() : null;
-            },
-            unsubscribe(){
-                this.unsubscribed = true;
-            }
+  
+  constructor(setup) {
+    this._setup = setup;
+  }
+ 
+  subscribe(subscriber) {
+    const wrapper = {
+        isUnsubscrive: false,
+        next(val){
+          if(this.isUnsubscrive) return;
+          if(typeof subscriber === 'function') return subscriber(val)
+          return subscriber.next ? subscriber.next(val) : null;
+        },
+        error(val){
+          if(this.isUnsubscrive) return;
+          this.unsubscribe();
+          return subscriber.error ? subscriber.error(val) : null;
+        },
+        complete(){
+          if(this.isUnsubscrive) return;
+          this.unsubscribe();
+          return subscriber.complete ? subscriber.complete() : null;
+        },
+        unsubscribe(){
+          this.isUnsubscrive = true;
         }
-
-        this._setup(sub_wrapper); 
-        return sub_wrapper;
     }
+
+    this._setup(wrapper);
+    return wrapper;
+  }
 }
