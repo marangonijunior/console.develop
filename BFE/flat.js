995ae5a25834d3828f9248aacb0e434e5d8efa54
Hednei C Marangoni Junior <marangonijunior@gmail.com>
2022-03-09 18:21:38 +0000

    Update flat.js

--- BFE/flat.js
+++ BFE/flat.js
@@ -1,22 +1,22 @@
-const arr = [1, [2], [3, [4]]];
 
-const flat = (arr, level = 1) => {
-    let result = [];
-    let isDone = true;
+/**
+ * @param { Array } arr
+ * @param { number } depth
+ * @returns { Array }
+ */
+function flat(arr, depth = 1) {
+  let result = [];
+  let isDone = true;
 
-    for(let item of arr){
-        if(!Array.isArray(item)){
-           result.push(item);
-        } else {
-           isDone = false
-           result.push(...item);
-        }
+  for(item of arr){
+    if(!Array.isArray(item)){
+      result.push(item);
+    } else {
+      isDone = false;
+      result.push(...item);
     }
-    
-    return level === 0 || isDone ? arr : flat(result, level - 1)
-}
-    
+  }
 
-console.log(flat(arr, Infinity))
-console.log(flat(arr, 1))
+  return depth === 0 || isDone ? arr : flat(result, depth - 1)
+}
 
