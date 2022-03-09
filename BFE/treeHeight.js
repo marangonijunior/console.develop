f254764ca814f4fc1f49ad32961301aca611440f
Hednei C Marangoni Junior <marangonijunior@gmail.com>
2022-03-09 18:39:18 +0000

    Update treeHeight.js

--- BFE/treeHeight.js
+++ BFE/treeHeight.js
@@ -1,47 +1,19 @@
-class Emitter {
-  constructor() {
-      this.queue = [];
+
+/**
+ * @param {HTMLElement | null} tree
+ * @return {number}
+ */
+function getHeight(tree) {
+  let max = 0;
+
+  if(!tree){
+    return max;
+  }     
+
+  for(let item of tree.children){
+    max = Math.max(max, getHeight(item));
   }
 
-  subscribe(event, fn){
-      if (this.queue[event]) {
-          this.queue[event].push(fn);
-      } else {
-          this.queue[event] = [fn];
-      }
-      return {
-          release: () => {
-              const fncs = this.queue[event];
-              const idx = fncs.indexOf(fn);
-              if(idx > -1){
-                  fncs.splice(idx, 1);
-              }
-          }
-      }
-  }
 
-  emit(event, ...args){
-      this.queue[event].forEach(item => {
-          item.apply(null, args)
-      })
-  }
+  return max + 1;
 }
-
-const square = (num) => {
-  console.log(`Or number squared ${num * 2}`)
-}
-
-const sum = (num_a, num_b) => {
-  console.log(`Or sum ${num_a + num_b}`)
-}
-
-let myEmitter = new Emitter();
-
-const letSquare = myEmitter.subscribe('square', square);
-const letSum = myEmitter.subscribe('sum', sum);
-
-myEmitter.emit('square', 2);
-myEmitter.emit('sum', 2, 3);
-
-letSquare.release();
-letSum.release();
